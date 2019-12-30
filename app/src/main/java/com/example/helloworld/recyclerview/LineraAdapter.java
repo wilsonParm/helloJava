432aadf5716c3c812c755c23f51a542e63c8cc18
wilson <wilson@nanoleaf.me>
2019-12-30 21:50:10 +0800

    新增多类型recycler view

--- app/src/main/java/com/example/helloworld/recyclerview/LineraAdapter.java
+++ app/src/main/java/com/example/helloworld/recyclerview/LineraAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.helloworld.R;
 
-public class LineraAdapter extends RecyclerView.Adapter<LineraAdapter.LinearViewHolder> {
+public class LineraAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context mContext;
     private OnItemClickListener mlistener;
@@ -27,14 +28,21 @@ public class LineraAdapter extends RecyclerView.Adapter<LineraAdapter.LinearView
     }
 
     @Override
-    public LineraAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-        return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item, parent,false));
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == 0){
+            return new LinearViewHolder(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item, parent,false));
+        }else{
+            return new LinearViewHolder_2(LayoutInflater.from(mContext).inflate(R.layout.layout_linear_item_2, parent,false));
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LineraAdapter.LinearViewHolder holder, final int position) {
-        holder.textView.setText("hello World");
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        if(getItemViewType(position) == 0){
+            ((LinearViewHolder)holder).textView.setText("hello World");
+        }else {
+            ((LinearViewHolder_2) holder).textView.setText("o 哇哈哈哈哈哈");
+        }
         holder.itemView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -49,6 +57,16 @@ public class LineraAdapter extends RecyclerView.Adapter<LineraAdapter.LinearView
             }
         });
     }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(position % 2 == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
     @Override
     public int getItemCount() {
         return 30;
@@ -61,6 +79,17 @@ public class LineraAdapter extends RecyclerView.Adapter<LineraAdapter.LinearView
         public LinearViewHolder(View itemView){
             super(itemView);
             textView = itemView.findViewById(R.id.tv_title);
+        }
+    }
+    class LinearViewHolder_2 extends RecyclerView.ViewHolder{
+
+        private TextView textView;
+        private ImageView imageView;
+
+        public LinearViewHolder_2(View itemView){
+            super(itemView);
+            textView = itemView.<TextView>findViewById(R.id.tv_title);
+            imageView = itemView.<ImageView>findViewById(R.id.iv_image);
         }
     }
 
