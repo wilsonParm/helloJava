742eed978d86d72e71c268ffd79478d49cf918c0
Wilson <Wilson@nanoleaf.me>
2020-03-01 09:40:25 +0800

    fragment ： 回退栈

--- app/src/main/java/com/example/helloworld/fragment/AFragment.java
+++ app/src/main/java/com/example/helloworld/fragment/AFragment.java
@@ -3,9 +3,11 @@ package com.example.helloworld.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,6 +20,8 @@ public class AFragment extends Fragment {
 
     private TextView mtvTitle;
     private Activity mActivity;
+    private Button mBtnChange, mBtnReset;
+    private BFragment bFragment;
 
     public static AFragment newInstance(String title){
         AFragment fragment = new AFragment();
@@ -31,13 +35,36 @@ public class AFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_a, container, false);
+        Log.d("AFragment","----onCreateView");
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mtvTitle = view.findViewById(R.id.tv_title);
+        mtvTitle = (TextView) view.findViewById(R.id.tv_title);
+        mBtnChange = (Button) view.findViewById(R.id.btn_change);
+        mBtnReset = view.<Button>findViewById(R.id.btn_reset);
+        mBtnChange.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(bFragment == null){
+                    bFragment = new BFragment();
+                }
+                Fragment fragment = getFragmentManager().findFragmentByTag("a");
+                if(fragment != null){
+                    getFragmentManager().beginTransaction().hide(fragment).add(R.id.fl_container,bFragment).addToBackStack(null).commitAllowingStateLoss();
+                }else{
+                    getFragmentManager().beginTransaction().replace(R.id.fl_container,bFragment).addToBackStack(null).commitAllowingStateLoss();
+                }
+            }
+        }));
+        mBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mtvTitle.setText("new title");
+            }
+        });
         if(getArguments() != null){
             mtvTitle.setText(getArguments().getString("title"));
         }
--- app/src/main/java/com/example/helloworld/fragment/ContainerActivity.java
+++ app/src/main/java/com/example/helloworld/fragment/ContainerActivity.java
@@ -18,22 +18,12 @@ public class ContainerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_container);
-        mBtnChange = findViewById(R.id.btn_change);
-        mBtnChange.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                if(bFragment == null){
-                    bFragment = new BFragment();
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.fl_container, bFragment).commitAllowingStateLoss();
-            }
-        });
 
         //实例化AFragment
 //        aFragment = new AFragment();
         aFragment = AFragment.newInstance("我是传参");//新写的方法，里面已经包括了实例化的new
         //把AFragment添加到Activity中, 记得调用commit
-        getSupportFragmentManager().beginTransaction().add(R.id.fl_container, aFragment).commitAllowingStateLoss();
+        getSupportFragmentManager().beginTransaction().add(R.id.fl_container, aFragment,"a").commitAllowingStateLoss();
 
     }
 }
